98bc0402d0e36b94e03bcf2b7172564b7f36df49
Michael Shafae <mshafae@fullerton.edu>
2013-03-05 22:01:36 -0800

    updated

---
+++ MoveBetweenTextFields/MoveBetweenTextFields/UITextField+NextTextFieldProperty.h
@@ -0,0 +1,13 @@
+//
+//  UITextField+NextTextFieldProperty.h
+//  MoveBetweenTextFields
+//
+//  Created by Michael Shafae on 3/5/13.
+//  Copyright (c) 2013 Michael Shafae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (NextTextFieldProperty)
+
+@end
---
+++ MoveBetweenTextFields/MoveBetweenTextFields/UITextField+NextTextFieldProperty.m
@@ -0,0 +1,13 @@
+//
+//  UITextField+NextTextFieldProperty.m
+//  MoveBetweenTextFields
+//
+//  Created by Michael Shafae on 3/5/13.
+//  Copyright (c) 2013 Michael Shafae. All rights reserved.
+//
+
+#import "UITextField+NextTextFieldProperty.h"
+
+@implementation UITextField (NextTextFieldProperty)
+
+@end
