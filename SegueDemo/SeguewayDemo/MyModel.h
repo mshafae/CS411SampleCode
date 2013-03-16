2408ded1ea5fa7efcf99919079c9a4eedd18f33b
Michael Shafae <mshafae@fullerton.edu>
2013-03-15 17:57:15 -0700

    fixed most of the errors and pass a model between the two view controllers

---
+++ SegueDemo/SeguewayDemo/MyModel.h
@@ -0,0 +1,13 @@
+//
+//  MyModel.h
+//  SeguewayDemo
+//
+//  Created by Michael Shafae on 3/15/13.
+//  Copyright (c) 2013 Michael Shafae. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyModel : NSObject
+
+@end
---
+++ SegueDemo/SeguewayDemo/MyModel.m
@@ -0,0 +1,13 @@
+//
+//  MyModel.m
+//  SeguewayDemo
+//
+//  Created by Michael Shafae on 3/15/13.
+//  Copyright (c) 2013 Michael Shafae. All rights reserved.
+//
+
+#import "MyModel.h"
+
+@implementation MyModel
+
+@end
